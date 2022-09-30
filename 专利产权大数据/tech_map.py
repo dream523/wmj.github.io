6913aa2844457dbe9e8737e2c4e49504b6fd51bc
wangmj <wangmj@quantditech.com>
2022-09-30 09:17:54 +0800

    上传文件

--- 专利产权大数据/tech_map.py
+++ 专利产权大数据/tech_map.py
@@ -13,12 +13,12 @@ def is_uchar(uchar):
             return False
     """判断一个unicode是否是数字"""
     if uchar >= u'\u0030' and uchar<=u'\u0039':
-            return True
+            return False
     """判断一个unicode是否是英文字母"""
     if (uchar >= u'\u0041' and uchar<=u'\u005a') or (uchar >= u'\u0061' and uchar<=u'\u007a'):
             return True
     if uchar in ('-'):
-            return True
+            return False
     return False
 
 ###################*************************TCAD*************************###################
